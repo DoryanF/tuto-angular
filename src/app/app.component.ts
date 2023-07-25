7df358d78d546282873b8370f6bbe3fdf484d99f
DoryanF <doryan.fourrichon@hotmail.fr>
2023-07-25 09:49:20 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cda_maximillian_tuto_udemy';
+}
