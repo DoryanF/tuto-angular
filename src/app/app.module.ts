95dbae7d20038e2a74ec923cb5061d8fbf15d420
DoryanF <doryan.fourrichon@hotmail.fr>
2023-07-28 14:02:16 +0200

    second commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { ExerciceCalculComponent } from './exercice-calcul/exercice-calcul.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './users/user/user.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    ExerciceCalculComponent
+    ExerciceCalculComponent,
+    HomeComponent,
+    UserComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule,
